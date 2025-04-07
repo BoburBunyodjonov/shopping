39d40b37402da6b35e4bff3d97959ee7d4f56914
Bobur Bunyodjonov <80701998+BoburBunyodjonov@users.noreply.github.com>
2025-04-08 00:02:03 +0500

    bug fixed

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -9,7 +9,7 @@ const Products: React.FC = () => {
     id: i + 1,
     name: `Mahsulot ${i + 1}`,
     price: `${(i + 1) * 10} 000 So'm`,
-    image: `/src/assets/product/product${i + 1}.png`,
+    image: `/assets/product/product${i + 1}.png`, // Ensure images are in the public/assets/product folder
   }));
 
   return (
