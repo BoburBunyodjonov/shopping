2fc12d14b3fb7ed34c599838e6b00396b4ff62df
Bobur Bunyodjonov <80701998+BoburBunyodjonov@users.noreply.github.com>
2025-04-13 02:51:21 +0500

    fixed

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,20 +4,23 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 import Carousel from "./Carousel";
 import Map from "./Map";
+import { AuthProvider } from "../AuthContext";
 
 const Layout: React.FC = () => {
   const location = useLocation();
 
   return (
     <div>
-      <Navbar logoText="Bordo" cartCount={0} />
-      {/* Render Carousel only on the Home page */}
-      {location.pathname === "/" && <Carousel />}
-      <main className="p-4">
-        <Outlet />
-      </main>
-      {location.pathname === "/" && <Map />} 
-      <Footer />
+      <AuthProvider>
+        <Navbar logoText="Bordo" cartCount={0} />
+        {/* Render Carousel only on the Home page */}
+        {location.pathname === "/" && <Carousel />}
+        <main className="p-4">
+          <Outlet />
+        </main>
+        {location.pathname === "/" && <Map />}
+        <Footer />
+      </AuthProvider>
     </div>
   );
 };
